321ffc7dddedb2e2bf2509867bf4126907c841f4
JibayMcs <jb@ss2i-digital.fr>
2024-11-28 12:03:24 +0100

    (+) [Clear Form Button] Reordered clear form button if there's next pages

--- resources/views/forms/surveyjs-form-field.blade.php
+++ resources/views/forms/surveyjs-form-field.blade.php
@@ -364,6 +364,14 @@
                 </button>
             @endif
 
+                <button style="--c-400: var(--gray-400); --c-500: var(--gray-500); --c-600: var(--gray-600);"
+                        class="fi-btn relative grid-flow-col items-center justify-center font-semibold outline-none transition duration-75 focus-visible:ring-2 rounded-lg fi-color-custom fi-btn-color-success fi-color-success fi-size-md fi-btn-size-md gap-1.5 px-3 py-2 text-sm inline-grid shadow-sm bg-custom-600 text-white hover:bg-custom-500 focus-visible:ring-custom-500/50 dark:bg-custom-500 dark:hover:bg-custom-400 dark:focus-visible:ring-custom-400/50"
+                        type="button"
+                        @click="clearForm"
+                >
+                    <span class="fi-btn-label">Effacer le formulaire</span>
+                </button>
+
             @if($field->showNextButton && $field->showButtons)
                 <button style="--c-400:var(--primary-400);--c-500:var(--primary-500);--c-600:var(--primary-600);"
                         class="fi-btn relative grid-flow-col items-center justify-center font-semibold outline-none transition duration-75 focus-visible:ring-2 rounded-lg fi-color-custom fi-btn-color-primary fi-color-primary fi-size-md fi-btn-size-md gap-1.5 px-3 py-2 text-sm inline-grid shadow-sm bg-custom-600 text-white hover:bg-custom-500 focus-visible:ring-custom-500/50 dark:bg-custom-500 dark:hover:bg-custom-400 dark:focus-visible:ring-custom-400/50"
@@ -393,14 +401,6 @@
                     @click="redirectBack"
             >
                 <span class="fi-btn-label">Retour à la liste</span>
-            </button>
-
-            <button style="--c-400: var(--gray-400); --c-500: var(--gray-500); --c-600: var(--gray-600);"
-                    class="fi-btn relative grid-flow-col items-center justify-center font-semibold outline-none transition duration-75 focus-visible:ring-2 rounded-lg fi-color-custom fi-btn-color-success fi-color-success fi-size-md fi-btn-size-md gap-1.5 px-3 py-2 text-sm inline-grid shadow-sm bg-custom-600 text-white hover:bg-custom-500 focus-visible:ring-custom-500/50 dark:bg-custom-500 dark:hover:bg-custom-400 dark:focus-visible:ring-custom-400/50"
-                    type="button"
-                    @click="clearForm"
-            >
-                <span class="fi-btn-label">Effacer le formulaire</span>
             </button>
         </div>
     </template>
